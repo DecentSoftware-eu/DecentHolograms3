1c0d659684a193a3538042cbe6169d86953e136d
d0by <dobbyvoe@gmail.com>
2022-06-16 23:17:28 +0200

    Implement IconContentParser.java

---
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/components/line/content/IconContentParser.java
@@ -0,0 +1,27 @@
+package eu.decentsoftware.holograms.components.line.content;
+
+import eu.decentsoftware.holograms.api.component.line.Line;
+import eu.decentsoftware.holograms.api.component.line.LineRenderer;
+import eu.decentsoftware.holograms.api.component.line.content.ContentParser;
+import eu.decentsoftware.holograms.api.utils.item.LineItemStack;
+import eu.decentsoftware.holograms.components.line.renderer.IconLineRenderer;
+import org.jetbrains.annotations.NotNull;
+
+public class IconContentParser implements ContentParser {
+
+    @Override
+    public boolean parse(@NotNull Line line) {
+        String content = line.getContent();
+        if (!content.startsWith("#ICON:")) {
+            return false;
+        }
+        LineItemStack itemStack = LineItemStack.fromString(content);
+        if (itemStack == null) {
+            return false;
+        }
+        LineRenderer renderer = new IconLineRenderer(line, itemStack);
+        line.setRenderer(renderer);
+        return true;
+    }
+
+}
