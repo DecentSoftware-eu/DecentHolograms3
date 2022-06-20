9b2448f4194de725afa5a58b493548ffc12159ad
d0by <dobbyvoe@gmail.com>
2022-06-20 21:34:56 +0200

    Implement SmallHeadContentParser.java

---
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/components/line/content/parsers/SmallHeadContentParser.java
@@ -0,0 +1,27 @@
+package eu.decentsoftware.holograms.components.line.content.parsers;
+
+import eu.decentsoftware.holograms.api.component.line.Line;
+import eu.decentsoftware.holograms.api.component.line.LineRenderer;
+import eu.decentsoftware.holograms.api.component.line.content.ContentParser;
+import eu.decentsoftware.holograms.api.component.line.content.objects.LineItemStack;
+import eu.decentsoftware.holograms.components.line.renderer.SmallHeadLineRenderer;
+import org.jetbrains.annotations.NotNull;
+
+public class SmallHeadContentParser implements ContentParser {
+
+    @Override
+    public boolean parse(@NotNull Line line) {
+        String content = line.getContent();
+        if (!content.startsWith("#SMALLHEAD:")) {
+            return false;
+        }
+        LineItemStack itemStack = LineItemStack.fromString(content);
+        if (itemStack == null) {
+            return false;
+        }
+        LineRenderer renderer = new SmallHeadLineRenderer(line, itemStack);
+        line.setRenderer(renderer);
+        return true;
+    }
+
+}
