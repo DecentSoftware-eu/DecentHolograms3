1c2ddf43ff48dc58b0e620deb1de54c2831c1b6b
d0by <dobbyvoe@gmail.com>
2022-06-24 20:18:40 +0200

    Actions loading logic

--- decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/actions/DefaultAction.java
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/actions/DefaultAction.java
@@ -1,9 +1,20 @@
 package eu.decentsoftware.holograms.actions;
 
+import eu.decentsoftware.holograms.actions.impl.SoundAction;
+import eu.decentsoftware.holograms.actions.impl.StringAction;
+import eu.decentsoftware.holograms.actions.impl.StringListAction;
+import eu.decentsoftware.holograms.actions.impl.TeleportAction;
 import eu.decentsoftware.holograms.api.actions.Action;
+import eu.decentsoftware.holograms.api.config.ConfigUtils;
+import eu.decentsoftware.holograms.api.exception.LocationParseException;
 import lombok.Getter;
+import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 /**
  * This class represents an action, that can be executed for a specific profile.
@@ -54,9 +65,73 @@ public abstract class DefaultAction implements Action {
      * @param config The configuration section.
      * @return The action.
      */
-    public static DefaultAction load(@NotNull ConfigurationSection config) {
+    @Nullable
+    public static Action load(@NotNull ConfigurationSection config) {
+        if (!config.isString("type")) {
+            return null;
+        }
 
-        return null;
+        // -- Get type of the action
+        String typeName = config.getString("type", "");
+        ActionType type = ActionType.fromString(typeName);
+        if (type == null) {
+            return null;
+        }
+
+        // -- Create the action if possible
+        Action action = null;
+        switch (type) {
+            case SOUND:
+            case BROADCAST_SOUND:
+            case BROADCAST_SOUND_WORLD:
+                String soundName = config.getString("sound");
+                if (soundName != null) {
+                    float volume = (float) config.getDouble("volume", 1.0f);
+                    float pitch = (float) config.getDouble("pitch", 1.0f);
+                    try {
+                        Sound sound = Sound.valueOf(soundName.toUpperCase());
+                        action = new SoundAction(type, sound, volume, pitch);
+                    } catch (Exception ignored) {}
+                }
+                break;
+            case MESSAGE:
+            case BROADCAST:
+                List<String> stringList = config.getStringList("message");
+                if (!stringList.isEmpty()) {
+                    action = new StringListAction(type, stringList);
+                }
+                break;
+            case CHAT:
+                String message = config.getString("message");
+                if (message != null) {
+                    action = new StringAction(type, message);
+                }
+                break;
+            case COMMAND:
+            case CONSOLE:
+                String command = config.getString("command");
+                if (command != null) {
+                    action = new StringAction(type, command);
+                }
+                break;
+            case CONNECT:
+                String server = config.getString("server");
+                if (server != null) {
+                    action = new StringAction(type, server);
+                }
+                break;
+            case TELEPORT:
+                String locationString = config.getString("location");
+                if (locationString != null) {
+                    try {
+                        Location location = ConfigUtils.stringToLoc(locationString);
+                        action = new TeleportAction(location);
+                    } catch (LocationParseException ignored) {}
+                }
+                break;
+            default: break;
+        }
+        return action;
     }
 
 }
