3c4dae9ad3ca0be56d93a01a88e4cad2709a5640
d0by <dobbyvoe@gmail.com>
2022-06-25 09:48:58 +0200

    Cleanup

--- decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/pulsar/Pulsar.java
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/pulsar/Pulsar.java
@@ -4,9 +4,6 @@ import eu.decentsoftware.holograms.api.pulsar.IPulsar;
 import eu.decentsoftware.holograms.api.pulsar.PulseType;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * Implementation of {@link IPulsar}.
- */
 public class Pulsar implements IPulsar {
 
     @Override
