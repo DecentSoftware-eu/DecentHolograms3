e5d83c6484f764940d6887e446631f348d023390
d0by <dobbyvoe@gmail.com>
2022-07-03 18:13:29 +0200

    Remove unnecessary method, found a better way to do it

--- decent-holograms-api/src/main/java/eu/decentsoftware/holograms/api/component/hologram/HologramClickManager.java
+++ decent-holograms-api/src/main/java/eu/decentsoftware/holograms/api/component/hologram/HologramClickManager.java
@@ -1,7 +1,6 @@
 package eu.decentsoftware.holograms.api.component.hologram;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.ClickType;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -12,17 +11,6 @@ import org.jetbrains.annotations.NotNull;
  * @since 3.0.0
  */
 public interface HologramClickManager {
-
-    /**
-     * Attempt to handle a click on the given entityId. This method will try to find the
-     * correct line in this hologram and handle the click on it. If the line is not found,
-     * this method will return false.
-     *
-     * @param clickType The type of click.
-     * @param entityId The entityId of the clicked hologram.
-     * @return True if the click was handled, false otherwise.
-     */
-    boolean attemptHandleClick(@NotNull ClickType clickType, int entityId);
 
     /**
      * Attempt to detect the currently watched line by the given player. This method will try
