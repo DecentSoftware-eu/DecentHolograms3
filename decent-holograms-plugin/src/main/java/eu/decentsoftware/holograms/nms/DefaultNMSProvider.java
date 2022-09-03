6fdfdccc5ee42226e534a25b90c9e2f49c7aacad
d0by <dobbyvoe@gmail.com>
2022-09-03 16:12:13 +0200

    Implement NMS provider

--- decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/nms/DefaultNMSProvider.java
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/nms/DefaultNMSProvider.java
@@ -3,14 +3,26 @@ package eu.decentsoftware.holograms.nms;
 import eu.decentsoftware.holograms.api.nms.NMSAdapter;
 import eu.decentsoftware.holograms.api.nms.NMSProvider;
 import eu.decentsoftware.holograms.api.nms.listener.PacketListener;
+import eu.decentsoftware.holograms.api.utils.reflect.Version;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class DefaultNMSProvider implements NMSProvider {
 
-    private NMSAdapter adapter;
-    private PacketListener packetListener;
+    private final NMSAdapter adapter;
+    private final PacketListener packetListener;
 
-    public DefaultNMSProvider() {
-        // TODO: INIT
+    /**
+     * Initializes the NMS adapter. If the version is not supported, an exception is thrown.
+     *
+     * @throws IllegalStateException If the current version is not supported.
+     */
+    public DefaultNMSProvider() throws IllegalStateException {
+        if ((adapter = initNMSAdapter()) == null) {
+            throw new IllegalStateException(String.format("Version %s is not supported!", Version.CURRENT.name()));
+        }
+        packetListener = new PacketListener();
     }
 
     @Override
@@ -21,6 +33,24 @@ public class DefaultNMSProvider implements NMSProvider {
     @Override
     public PacketListener getPacketListener() {
         return packetListener;
+    }
+
+    /**
+     * Attempts to find the correct NMS adapter for the current version.
+     *
+     * @return The NMS adapter or null if none was found.
+     */
+    @Nullable
+    private NMSAdapter initNMSAdapter() {
+        String version = Version.CURRENT.name();
+        String className = "eu.decentsoftware.holograms.nms.NMSAdapter_" + version;
+        try {
+            Class<?> clazz = Class.forName(className);
+            return (NMSAdapter) clazz.getDeclaredConstructor().newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+                 InvocationTargetException e) {
+            return null;
+        }
     }
 
 }
