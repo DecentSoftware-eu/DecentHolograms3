c4a9d85e0c504e957a36f2f44caa95494ed92896
d0by <dobbyvoe@gmail.com>
2022-12-23 15:09:23 +0100

    NMS Adapter init logic fix

--- decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/nms/DefaultNMSProvider.java
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/nms/DefaultNMSProvider.java
@@ -4,6 +4,7 @@ import eu.decentsoftware.holograms.BootProcess;
 import eu.decentsoftware.holograms.api.nms.NMSAdapter;
 import eu.decentsoftware.holograms.api.nms.NMSProvider;
 import eu.decentsoftware.holograms.api.nms.listener.PacketListener;
+import eu.decentsoftware.holograms.api.utils.reflect.R;
 import eu.decentsoftware.holograms.api.utils.reflect.Version;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,49 +12,49 @@ import java.lang.reflect.InvocationTargetException;
 
 public class DefaultNMSProvider implements NMSProvider {
 
-    private final NMSAdapter adapter;
+	private final NMSAdapter adapter;
 //    private final PacketListener packetListener;
 
-    /**
-     * Initializes the NMS adapter. If the version is not supported, an exception is thrown.
-     *
-     * @throws IllegalStateException If the current version is not supported.
-     */
-    public DefaultNMSProvider() throws IllegalStateException {
-        if ((adapter = initNMSAdapter()) == null) {
-            throw new IllegalStateException(String.format("Version %s is not supported!", Version.CURRENT.name()));
-        }
+	/**
+	 * Initializes the NMS adapter. If the version is not supported, an exception is thrown.
+	 *
+	 * @throws IllegalStateException If the current version is not supported.
+	 */
+	public DefaultNMSProvider() throws IllegalStateException {
+		if ((adapter = initNMSAdapter()) == null) {
+			throw new IllegalStateException(String.format("Version %s is not supported!", Version.CURRENT.name()));
+		}
 //        packetListener = new PacketListener();
 
-        BootProcess.log("Using NMS adapter for your version: " + Version.CURRENT.name());
-    }
+		BootProcess.log("Using NMS adapter for your version: " + Version.CURRENT.name());
+	}
 
-    @Override
-    public NMSAdapter getAdapter() {
-        return adapter;
-    }
+	@Override
+	public NMSAdapter getAdapter() {
+		return adapter;
+	}
 
-    @Override
-    public PacketListener getPacketListener() {
-        return null;
-    }
+	@Override
+	public PacketListener getPacketListener() {
+		return null;
+	}
 
-    /**
-     * Attempts to find the correct NMS adapter for the current version.
-     *
-     * @return The NMS adapter or null if none was found.
-     */
-    @Nullable
-    private NMSAdapter initNMSAdapter() {
-        String version = Version.CURRENT.name();
-        String className = "eu.decentsoftware.holograms.nms.NMSAdapter_" + version;
-        try {
-            Class<?> clazz = Class.forName(className);
-            return (NMSAdapter) clazz.getDeclaredConstructor().newInstance();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
-                 InvocationTargetException e) {
-            return null;
-        }
-    }
+	/**
+	 * Attempts to find the correct NMS adapter for the current version.
+	 *
+	 * @return The NMS adapter or null if none was found.
+	 */
+	@Nullable
+	private NMSAdapter initNMSAdapter() {
+		String version = R.getVersion();
+		String className = "eu.decentsoftware.holograms.nms.NMSAdapter_" + version;
+		try {
+			Class<?> clazz = Class.forName(className);
+			return (NMSAdapter) clazz.getDeclaredConstructor().newInstance();
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+		         InvocationTargetException e) {
+			return null;
+		}
+	}
 
 }
