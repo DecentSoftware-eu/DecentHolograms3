03b657198f9cd74a25c4379f8e57c6944138b225
d0by <dobbyvoe@gmail.com>
2022-12-23 15:01:02 +0100

    MiniMessage parsing into IChatBaseComponent

--- decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/hooks/MiniMessageHook.java
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/hooks/MiniMessageHook.java
@@ -1,8 +1,30 @@
 package eu.decentsoftware.holograms.hooks;
 
 import lombok.experimental.UtilityClass;
+import net.kyori.adventure.platform.bukkit.MinecraftComponentSerializer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * This class provides methods for using MiniMessage.
+ *
+ * @author d0by
+ * @since 3.0.0
+ */
 @UtilityClass
 public final class MiniMessageHook {
+
+    /**
+     * Serialize the given string to a IChatBaseComponent using MiniMessage.
+     *
+     * @param string The string to serialize.
+     * @return The serialized IChatBaseComponent.
+     */
+    @NotNull
+    public static Object serializeMinecraft(@NotNull String string) {
+        Component component = MiniMessage.miniMessage().deserialize(string);
+        return MinecraftComponentSerializer.get().serialize(component);
+    }
 
 }
