6de95872f5340384fe028aab74bf71bdd5ddbce1
d0by <dobbyvoe@gmail.com>
2023-02-24 00:23:53 +0100

    Properly updating hologram visibility on player death and teleport

--- decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/listener/PlayerListener.java
+++ decent-holograms-plugin/src/main/java/eu/decentsoftware/holograms/listener/PlayerListener.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package eu.decentsoftware.holograms.profile;
+package eu.decentsoftware.holograms.listener;
 
 import eu.decentsoftware.holograms.Config;
 import eu.decentsoftware.holograms.DecentHolograms;
@@ -24,15 +24,18 @@ import eu.decentsoftware.holograms.Lang;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 /**
- * This listener handles some events related to the player profiles.
+ * This listener handles some events related to the players.
  *
  * @author d0by
  * @since 3.0.0
  */
-public class ProfileListener implements Listener {
+public class PlayerListener implements Listener {
 
     private static final DecentHolograms PLUGIN = DecentHolograms.getInstance();
 
@@ -40,7 +43,7 @@ public class ProfileListener implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         PLUGIN.getProfileRegistry().registerProfile(player.getUniqueId());
-//        PLUGIN.getNMSProvider().getPacketListener().hook(player);
+        PLUGIN.getNMSManager().hook(player);
 
         // -- Notify the player about a new version available
         if (Config.isUpdateAvailable() && player.hasPermission(Config.ADMIN_PERM)) {
@@ -52,7 +55,27 @@ public class ProfileListener implements Listener {
     public void onQuit(PlayerQuitEvent e) {
         Player player = e.getPlayer();
         PLUGIN.getProfileRegistry().removeProfile(player.getUniqueId());
-//        PLUGIN.getNMSProvider().getPacketListener().unhook(player);
+        PLUGIN.getNMSManager().unhook(player);
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent e) {
+        Player player = e.getPlayer();
+        PLUGIN.getHologramRegistry().getHolograms().forEach((hologram) -> {
+            // Hide all holograms for the player. They will be shown again immediately
+            // but this will prevent the holograms from becoming invisible for the player.
+            hologram.getVisibilityManager().updateVisibility(player, false);
+        });
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerDeathEvent e) {
+        Player player = e.getEntity();
+        PLUGIN.getHologramRegistry().getHolograms().forEach((hologram) -> {
+            // Hide all holograms for the player. They will be shown again immediately
+            // but this will prevent the holograms from becoming invisible for the player.
+            hologram.getVisibilityManager().updateVisibility(player, false);
+        });
     }
 
 }
