0b12b1cf7397a025d7f63472f1618d0472b623a2
Dávid Malich <david.malich2@gmail.com>
2023-03-08 22:37:36 +0100

    Added @AddonNameDefinition annotation for defining addon name

---
+++ decent-holograms-api/src/main/java/eu/decentsoftware/holograms/api/addons/AddonNameDefinition.java
@@ -0,0 +1,12 @@
+package eu.decentsoftware.holograms.api.addons;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AddonNameDefinition {
+    String value();
+}
